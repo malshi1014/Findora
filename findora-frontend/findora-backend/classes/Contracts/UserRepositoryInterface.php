97d16076427a98d8e77b23e13d8ce3d202c5bcc2
Malshi <malshinavodya14@gmail.com>
2026-08-15 22:14:27 +0530

    Remove findora-backend from inside findora-frontend

--- findora-frontend/findora-backend/classes/Contracts/UserRepositoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// OOP: polymorphic repository contract.
-interface UserRepositoryInterface
-{
-    public function findByLoginId(string $loginId): ?array;
-    public function findUsersByNearestTown(string $town, int $excludeUserId): array;
-}
